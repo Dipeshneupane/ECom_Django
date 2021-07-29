54b439667fdb51a306afc98e0d96887a2a3afdf4
Dipesh Neupane <dipeshneupane2055@gmail.com>
2021-07-29 13:59:27 +0545

    model created

--- ecomproject/ecomapp/views.py
+++ ecomproject/ecomapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import *
 
 # Create your views here.
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["myname"] = "Dipesh Neupane" 
+        context["product_list"] = Product.objects.all()
+        return context
+    
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+class ContactView(TemplateView):
+    template_name = "contact.html"
+
